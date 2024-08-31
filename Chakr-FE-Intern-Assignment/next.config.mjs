7d48d1ae28d8679093f39345630fe8ba0362f5e1
Ishika jain <114659283+Ij26@users.noreply.github.com>
2024-08-31 23:42:57 +0530

    Update next.config.mjs

--- Chakr-FE-Intern-Assignment/next.config.mjs
+++ Chakr-FE-Intern-Assignment/next.config.mjs
@@ -4,6 +4,17 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   assetPrefix: isProd ? "/ChakrFE.github.io/" : "",
   basePath: isProd ? "/ChakrFE.github.io" : "",
+  target: 'serverless',
+  // Uncomment the following line if you are using `next export` to make a fully static site
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     // Add other paths here
+  //   };
+  // },
 };
 
 export default nextConfig;
